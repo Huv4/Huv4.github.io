117915566d6f1290d63b2c885a28213b469f54d3
felix <felix.dressler@eas.iis.fraunhofer.de>
2023-04-23 22:22:30 +0200

    Implementation of Php

---
+++ 01_Moody.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8" name="viewport" content="width=device-width,initial-scale=1.0">
+  <link rel="stylesheet" href="css/styles.css">
+  <script src="https://kit.fontawesome.com/26297b1dd1.js" crossorigin="anonymous"></script>
+  <title>Moody - Felix Dressler</title>
+</head>
+
+<body>
+  <h1>
+    FELIX DRESSLER.
+  </h1>
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
--- fiveserver.config.js
+++ fiveserver.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   highlight: true,
-  injectBody: true
+  injectBody: true,
+  open: "index.html",
+  php: "C:\\Program Files\\php-8.2.5-nts-Win32-vs16-x64\\php.exe"
 };
