6be5397225740dad1b31ea18f859c8eda34ac2a6
felix <felix.dressler@eas.iis.fraunhofer.de>
2023-05-31 18:59:22 +0200

    working masonry

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 const gallery = document.getElementById("photo-gallery");
 const popup = document.getElementById("popup");
 const selectedImage = document.getElementById("selectedImage");
+const column1 = document.getElementById("column1");
+const column2 = document.getElementById("column2");
+const column3 = document.getElementById("column3");
 const selectedIndex = null;
 
 //only run this script when on Photo Gallery Pages
@@ -10,8 +13,12 @@ if (typeof (gallery) != "undefined" && gallery != null) {
     .then(Response => Response.json())
     .then(data => {
       [0, 1].forEach(e => delete data[e]); //delete first two Elements from json ([.] and [..])
+      const columns = [column1, column2, column3]; //array to iterate over columns
+      let columnIndex = 0;
       data.forEach(i => {
         console.log("Test der Console");
+        const photo = document.createElement("div");
+        photo.classList.add("photo")
         const image = document.createElement("img");
         image.src = `/PHP/uploads/${i}`;
         image.alt = "A picture. Probably beautiful.";
@@ -23,8 +30,11 @@ if (typeof (gallery) != "undefined" && gallery != null) {
           selectedImage.src = `/PHP/uploads/${i}`;
           selectedImage.alt = "A picture. Probably beautiful.";
         });
-
-        gallery.appendChild(image);
+        //Append the photo to the current column
+        columns[columnIndex].appendChild(photo);
+        photo.appendChild(image);
+        //update the column index for the next iteration
+        columnIndex = (columnIndex + 1) % columns.length;
       });
     });
 } else {
