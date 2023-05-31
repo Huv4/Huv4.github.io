2f386ba0a1fd3560ce946db59b91b7e4d4145b42
felix <felix.dressler@eas.iis.fraunhofer.de>
2023-05-31 22:47:54 +0200

    Deleted PHP -> moved to imagekit

--- js/main.js
+++ js/main.js
@@ -2,41 +2,37 @@
 const gallery = document.getElementById("photo-gallery");
 const popup = document.getElementById("popup");
 const selectedImage = document.getElementById("selectedImage");
+const imageIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+const selectedIndex = null;
 const column1 = document.getElementById("column1");
 const column2 = document.getElementById("column2");
 const column3 = document.getElementById("column3");
-const selectedIndex = null;
+const columns = [column1, column2, column3]; //array to iterate over columns
+let columnIndex = 0;
 
 //only run this script when on Photo Gallery Pages
 if (typeof (gallery) != "undefined" && gallery != null) {
-  fetch("/PHP/get_images.php")
-    .then(Response => Response.json())
-    .then(data => {
-      [0, 1].forEach(e => delete data[e]); //delete first two Elements from json ([.] and [..])
-      const columns = [column1, column2, column3]; //array to iterate over columns
-      let columnIndex = 0;
-      data.forEach(i => {
-        console.log("Test der Console");
-        const photo = document.createElement("div");
-        photo.classList.add("photo")
-        const image = document.createElement("img");
-        image.src = `/PHP/uploads/${i}`;
-        image.alt = "A picture. Probably beautiful.";
-        image.classList.add("galleryImg");
+  imageIndex.forEach((i) => {
+    console.log("Test der Console");
+    const photo = document.createElement("div");
+    photo.classList.add("photo")
+    const image = document.createElement("img");
+    image.src = `https://ik.imagekit.io/photoggang/image${i}.jpg`;
+    image.alt = "A picture. Probably beautiful.";
+    image.classList.add("galleryImg");
 
-        image.addEventListener("click", () => {
-          //popup stuff
-          popup.style.transform = `translateY(0)`;
-          selectedImage.src = `/PHP/uploads/${i}`;
-          selectedImage.alt = "A picture. Probably beautiful.";
-        });
-        //Append the photo to the current column
-        columns[columnIndex].appendChild(photo);
-        photo.appendChild(image);
-        //update the column index for the next iteration
-        columnIndex = (columnIndex + 1) % columns.length;
-      });
+    image.addEventListener("click", () => {
+      //popup stuff
+      popup.style.transform = `translateY(0)`;
+      selectedImage.src = `/PHP/uploads/${i}`;
+      selectedImage.alt = "A picture. Probably beautiful.";
     });
+    //Append the photo to the current column
+    columns[columnIndex].appendChild(photo);
+    photo.appendChild(image);
+    //update the column index for the next iteration
+    columnIndex = (columnIndex + 1) % columns.length;
+  });
 } else {
   console.log("Not on Photo Gallery Page.S");
 }
