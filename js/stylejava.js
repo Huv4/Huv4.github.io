a2e34eeec6eed802dc986c057524978926fa4acf
felix <felix.dressler@eas.iis.fraunhofer.de>
2023-06-03 12:36:33 +0200

    click for responsive

--- js/stylejava.js
+++ js/stylejava.js
@@ -1,15 +1,12 @@
-const dropdown = document.getElementById('dropdown-content');
 const category = document.getElementById('categoryMenuContent');
-const styleElement = document.getElementById('styleElement');
-
-function transformHeightdropdown() {
-  dropdown.style.height = "10rem";
-}
 
 function displayCategory() {
   category.style.display = "flex";
-  styleElement.style.display = "none";
+};
+
+function hideCategory() {
+  category.style.display = "none";
 }
 
-document.getElementById("dropdown").addEventListener('click', transformHeight);
 document.getElementById("categoryMenu").addEventListener('click', displayCategory);
+document.getElementById("categoryMenu").addEventListener('mouseleave', hideCategory);
