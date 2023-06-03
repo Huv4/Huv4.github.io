38a9506f4ef15cad43e8caa4a522d1d98a77b2b9
felix <felix.dressler@eas.iis.fraunhofer.de>
2023-06-03 14:00:26 +0200

    working click and hover effects with js

--- js/stylejava.js
+++ js/stylejava.js
@@ -1,4 +1,5 @@
 const category = document.getElementById('categoryMenuContent');
+const dropdown = document.getElementById("nameDropdownContent");
 
 function displayCategory() {
   category.style.display = "flex";
@@ -6,7 +7,19 @@ function displayCategory() {
 
 function hideCategory() {
   category.style.display = "none";
-}
+};
+
+function displayNames() {
+  dropdown.style.height = "10rem";
+};
+
+function hideNames() {
+  dropdown.style.height = "0";
+};
 
 document.getElementById("categoryMenu").addEventListener('click', displayCategory);
+document.getElementById("categoryMenu").addEventListener('mouseover', displayCategory);
 document.getElementById("categoryMenu").addEventListener('mouseleave', hideCategory);
+document.getElementById("nameDropdown").addEventListener('click', displayNames);
+document.getElementById("nameDropdown").addEventListener('mouseover', displayNames);
+document.getElementById("nameDropdown").addEventListener('mouseleave', hideNames);
