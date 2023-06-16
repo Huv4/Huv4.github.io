02b44fc68909592fb5a371d982aae68391b61e2e
felix <felix.dressler@eas.iis.fraunhofer.de>
2023-06-16 08:10:19 +0200

    new menu for categories; works for Felix Portfolio

--- js/stylejava.js
+++ js/stylejava.js
@@ -1,15 +1,4 @@
-const category = document.getElementById('categoryMenuContent');
 const dropdown = document.getElementById("nameDropdownContent");
-
-function displayCategory() {
-  category.style.visibility = "visible";
-  category.style.opacity = "1";
-};
-
-function hideCategory() {
-  category.style.visibility = "hidden";
-  category.style.opacity = "0";
-};
 
 function displayNames() {
   dropdown.style.height = "10rem";
@@ -19,9 +8,6 @@ function hideNames() {
   dropdown.style.height = "0";
 };
 
-document.getElementById("categoryMenu").addEventListener('click', displayCategory);
-document.getElementById("categoryMenu").addEventListener('mouseover', displayCategory);
-document.getElementById("categoryMenu").addEventListener('mouseleave', hideCategory);
 document.getElementById("nameDropdown").addEventListener('click', displayNames);
 document.getElementById("nameDropdown").addEventListener('mouseover', displayNames);
 document.getElementById("nameDropdown").addEventListener('mouseleave', hideNames);
